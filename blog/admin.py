7166b6f27664ad6b7dee1b1ece74fbe7852b1117
Harmeet Kaur <iharmeetk@gmail.com>
2025-01-16 15:37:29 +0000

    Update the blog app to use summernote and migrate

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
 from .models import Post, Comment
+from django_summernote.admin import SummernoteModelAdmin
+
+# register it with a decorator
+# ans then add the class which gives admin panel greater functionality and clarity
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'slug', 'status')
+    search_fields = ['title']
+    list_filter = ('status',)
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
+
 # Register your models here.
-admin.site.register(Post)
+
+# delete the existing Post model registration
+# admin.site.register(Post)
 admin.site.register(Comment)
