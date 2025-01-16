4eaed59902f4a8d6c30544d89719b00390cc0bd3
Harmeet Kaur <iharmeetk@gmail.com>
2025-01-16 15:28:38 +0000

    installed django_summernote, added it in req and installed apps

--- codestar/settings.py
+++ codestar/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'path('summernote/', include('django_summernote.urls')),',
     'blog',
 ]
 
